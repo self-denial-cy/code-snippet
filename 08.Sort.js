2ad965e052ba16173ed28139ad4d632d398c543f
Jerry <18556134754@163.com>
2024-06-12 00:15:45 +0800

    feat: insertionSort

---
+++ 08.Sort.js
@@ -0,0 +1,15 @@
+// 插入排序
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i]
+    let j = i - 1
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j]
+      j--
+    }
+    arr[j + 1] = current
+  }
+  return arr
+}
+
+console.log(insertionSort([5, 6, 1, 8, 7, 2, 4, 3]))
