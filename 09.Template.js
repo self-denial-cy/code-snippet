6e32a85edc10364d7ca50767d6397a55a84647a5
Jerry <18556134754@163.com>
2024-06-12 15:30:56 +0800

    feat: Template

--- 09.Template.js
+++ 09.Template.js
@@ -1,1 +1,14 @@
 // 简易模板引擎
+function render(tpl, data) {
+  return tpl.replace(/(\{\{(.+?)\}\})|(\{(.+?)\})/g, function ($1, $2, $3, $4, $5) {
+    if ($3) return data[$3]
+    return data[$5]
+  })
+}
+
+const str = render('嗨，{{name}}您好，今天是星期{day}', {
+  name: '张三',
+  day: '三'
+})
+
+console.log(str)
